a73f92671c901ea9ed5239e2445adb8419e4d153
hzml <875580718@qq.com>
2020-06-23 21:19:42 +0800

    'init'

---
+++ src/main/java/com/hzml/app/SpringbootWebsocketIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.hzml.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebsocketIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebsocketIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hzml/app/SpringbootWebsocketIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzml.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebsocketIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
